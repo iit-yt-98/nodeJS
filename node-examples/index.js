a80e746766731d67834b2748715c4d4760581c96
Yagyesh Tripathi <ytripathi.10.98@gmail.com>
2021-11-09 10:33:38 +0530

    After adding the async touch to explain how async works in JS

--- node-examples/index.js
+++ node-examples/index.js
@@ -3,11 +3,16 @@ var rect = require('./rectangle')
 
 function solveRect(x , y){
     console.log("Solving for the value L is : " + x + " and B is : " + y)
-    if(x <= 0 || y <= 0){
-        console.log("You gotta be kidding me")
-    }else{
-        console.log("Area is " + rect.area(x , y) + " and Perimeter is " + rect.perimeter(x , y))
-    }
+    
+    rect(x , y , (err , rectangle) => {
+        if(err){
+            console.log("Error is :" + err.message)
+        }else{
+            console.log("Area of rectangle is " + rectangle.area())
+            console.log("Perimeter of rectangle is " + rectangle.perimeter())
+        }
+    })
+    console.log("After running the method")
 }
 
 
