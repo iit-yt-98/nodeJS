a9e1c8e15d0fedeb2e0c82f32118ab6ea4d7910a
Yagyesh Tripathi <ytripathi.10.98@gmail.com>
2021-11-09 22:42:51 +0530

    Node HTTP with some changes(experiments)

--- node-http/index.js
+++ node-http/index.js
@@ -7,14 +7,14 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req,res) => {
-    console.log("Request for "+req.url + " by method " + req.method);
+    console.log("Request for "+ req.url + " by method " + req.method);
 
     if(req.method == 'GET'){
         var fileUrl;
         if (req.url == '/') fileUrl = '/index.html';
         else fileUrl = req.url;
 
-        var filePath = path.resolve('./public' + fileUrl);
+        var filePath = path.resolve('./public' + fileUrl);  // yahan public mention kiya h issiliye public folder ke andar daalna h 
         const fileExt = path.extname(filePath);
 
         if(fileExt == '.html'){
@@ -27,15 +27,14 @@ const server = http.createServer((req,res) => {
                 }
                 res.statusCode = 200;
                 res.setHeader('Content-Type' , 'text/html');
-                fs.createReadStream(filePath).pipe(res);
+                fs.createReadStream(filePath).pipe(res);   //read the file and put that in the response pipe
             });
         }else{
             res.statusCode = 404;
             res.setHeader('Content-Type' , 'text/html');
             res.end("<html><body><h1> Error 404 : " + fileUrl + " is not an HTML file. </h1></body></html>");
         }
-    }
-    else{
+    }else{
         res.statusCode = 404;
         res.setHeader('Content-Type' , 'text/html');
         res.end("<html><body><h1> Error 404 : " + req.method + " is not supported </h1></body></html>");
@@ -43,6 +42,6 @@ const server = http.createServer((req,res) => {
 });
 
 
-server.listen(port , hostname , () => {
+server.listen(port , 'localhost' , () => {
     console.log(`Server running at http://${hostname}:${port}`)
 })
