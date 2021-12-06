35dbdca034fd006faf956a55b8269c43b84ad22b
Yagyesh Tripathi <ytripathi.10.98@gmail.com>
2021-12-06 19:48:39 +0530

    After integrating MongoDB with NodeJS and Express Server

--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Dishes = require('../models/dishes');
 
 
 const dishRouter = express.Router();
@@ -8,18 +11,27 @@ dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
 
-.all((req , res , next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type' , 'text/plain');
-    next();
-})
+
 
 .get((req , res , next) => {
-    res.end('Will send all the dishes to you!');
+    Dishes.find({})
+    .then( (dish) => {
+        res.statusCode= 200;
+        res.setHeader('Content-Type' , 'application/json');
+        res.json(dish);
+    } , (err) => next(err))
+    .catch( (err) => next.apply(err));
 })
 
 .post((req,res,next) => {
-    res.end('Will add the dish: ' + req.body.name + " with details: " + req.body.description);
+     Dishes.create(req.body)
+     .then( (dish) => {
+         console.log("Dish Created MF",dish);
+         res.statusCode = 200;
+         res.setHeader('Content-Type', 'application/json');
+         res.json(dish);
+     } , (err) => next(err))
+     .catch( (err) => next(err));
 })
 
 .put((req,res,next) => {
@@ -28,7 +40,13 @@ dishRouter.route('/')
 })
 
 .delete((req,res,next) => {
-    res.end("Deleting all the dishes!");
+    Dishes.remove({})
+    .then( (resp) => {
+        res.statusCode = 200;
+         res.setHeader('Content-Type', 'application/json');
+         res.json(resp);
+    }, (err) => next(err))
+    .catch( (err) => next(err));
 });
 
 
@@ -36,7 +54,14 @@ dishRouter.route('/:dishId')
 
 
 .get( (req, res , next) => {
-    res.end("Will send dish with id :" + req.params.dishId);
+    //res.end("Will send dish with id :" + req.params.dishId);
+    Dishes.findById(req.params.dishId)
+    .then( (dish) => {
+        res.statusCode= 200;
+        res.setHeader('Content-Type' , 'application/json');
+        res.json(dish);
+    } , (err) => next(err))
+    .catch( (err) => next.apply(err));
 } )
 
 .post( (req , res , next) => {
@@ -45,12 +70,29 @@ dishRouter.route('/:dishId')
 })
 
 .put((req,res,next) => {
-    res.write("Updating the dish: " + req.params.dishId + '\n');
-    res.end("Will update the dish: " + req.body.name + " with details " + req.body.description);
+    //res.write("Updating the dish: " + req.params.dishId + '\n');
+    //res.end("Will update the dish: " + req.body.name + " with details " + req.body.description);
+    Dishes.findByIdAndUpdate(req.params.dishId , {
+        $set: req.body
+    } , {new: true})
+    .then( (dish) => {
+        res.statusCode= 200;
+        res.setHeader('Content-Type' , 'application/json');
+        res.json(dish);
+    } , (err) => next(err))
+    .catch( (err) => next.apply(err));
 })
 
 .delete((req,res,next) => {
-    res.end("Deleting the dish: " + req.params.dishId);
+    //res.end("Deleting the dish: " + req.params.dishId);
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then( (resp) => {
+        res.statusCode = 200;
+         res.setHeader('Content-Type', 'application/json');
+         res.json(resp);
+    }, (err) => next(err))
+    .catch( (err) => next(err));
+
 });
 
 module.exports = dishRouter;
